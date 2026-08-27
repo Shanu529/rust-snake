c63c1ae36af482c13def3c2bc6168d91ac2bb1ac
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-27 10:19:34 +0530

    add game board

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+
+
+
+
+fn main(){
+    for row in 0..7 {
+        
+        if row ==0 || row == 6 {
+            println!("########################");
+        } else{
+            println!("#                      #")
+        }
+    }
+}
