d6e476228502d9395b07fadcee7c129899b2c136
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-27 10:41:54 +0530

    add snake movement and boundary

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
-
-
-
+use std::{thread, time::Duration};
 
 fn main() {
+    let mut snake_col = 10;
 
-    for row in 0..7{
+    for _ in 0..10 {
+        print!("\x1B[2J\x1B[1;1H");
 
-        for column in 0..21 {
-
-            if row == 0 || row == 6 {
-                print!("#");
-            } else if column == 0 || column == 20 {
-                print!("#");
-            } else{
-                print!(" ");
+        for row in 0..7 {
+            for column in 0..21 {
+                if row == 0 || row == 6 {
+                    print!("#");
+                } else if column == 0 || column == 20 {
+                    print!("#");
+                } else if row == 3 && column == snake_col {
+                    print!(">");
+                } else {
+                    print!(" ");
+                }
             }
 
+            println!();
         }
 
-        println!();
-    }    
+        if snake_col < 19 {
+            snake_col += 1;
+        }
+        
+        thread::sleep(Duration::from_millis(300));
+        
+    }
 }
