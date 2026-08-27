7d2fb5b3c9c710c0083ae490cf93e1895117de94
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-27 11:24:19 +0530

    refactor main function and enhance board drawing logic

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,71 @@
-use std::{thread, time::Duration};
+use std::{
+    io::{self, Write},
+    thread,
+    time::Duration,
+};
+
+use crossterm::{
+    cursor::{MoveTo, Hide, Show},
+    execute,
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn draw_board(snake_col: i32) {
+    let mut stdout = io::stdout();
+
+    execute!(
+        stdout,
+        MoveTo(0, 0),
+        Clear(ClearType::All)
+    )
+    .unwrap();
+
+    for row in 0..10 {
+        for column in 0..21 {
+            if row == 0 || row == 9 {
+                print!("#");
+            } else if column == 0 || column == 20 {
+                print!("#");
+            } else if row == 3 && column == snake_col {
+                print!(">");
+            } else {
+                print!(" ");
+            }
+        }
+
+        println!();
+    }
+
+    stdout.flush().unwrap();
+}
 
 fn main() {
-    let mut snake_col = 10;
+    let mut stdout = io::stdout();
 
-    for _ in 0..10 {
-        print!("\x1B[2J\x1B[1;1H");
+    execute!(
+        stdout,
+        EnterAlternateScreen,
+        Clear(ClearType::All),
+        Hide
+    )
+    .unwrap();
 
-        for row in 0..7 {
-            for column in 0..21 {
-                if row == 0 || row == 6 {
-                    print!("#");
-                } else if column == 0 || column == 20 {
-                    print!("#");
-                } else if row == 3 && column == snake_col {
-                    print!(">");
-                } else {
-                    print!(" ");
-                }
-            }
+    let mut snake_col = 1;
 
-            println!();
-        }
+    for _ in 0..20 {
+        draw_board(snake_col);
 
         if snake_col < 19 {
             snake_col += 1;
         }
-        
+
         thread::sleep(Duration::from_millis(300));
-        
     }
+
+    execute!(
+        stdout,
+        Show,
+        LeaveAlternateScreen
+    )
+    .unwrap();
 }
