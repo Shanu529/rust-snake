ae8e9a9e26eb11e12ab4a7a15970470fec4ac88c
Shanu Chhetri <shanu.chhetri529@gmail.com>
2026-08-27 10:27:47 +0530

    add dynamic game board

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@
 
 
 
+fn main() {
 
-fn main(){
-    for row in 0..7 {
-        
-        if row ==0 || row == 6 {
-            println!("########################");
-        } else{
-            println!("#                      #")
+    for row in 0..7{
+
+        for column in 0..21 {
+
+            if row == 0 || row == 6 {
+                print!("#");
+            } else if column == 0 || column == 20 {
+                print!("#");
+            } else{
+                print!(" ");
+            }
+
         }
-    }
+
+        println!();
+    }    
 }
